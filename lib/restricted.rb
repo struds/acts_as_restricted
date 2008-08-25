68d4b9ae857c209577e3fefd54b64b478a9bb8a5
Stephen Strudwick <stephen@strud.me.uk>
2008-08-25 16:52:25 +0100

    fixed to work in a rails app from controllers

--- lib/restricted.rb
+++ lib/restricted.rb
@@ -2,22 +2,27 @@ module Restricted
         module Dependencies
 
             def load_missing_constant(from, const_name)
-                if @loaded != const_name
-                    @loaded = const_name
-                    base_obj = Object.const_get(const_name)
 
+                if from == Restricted
+                    if ::Object.const_defined?(const_name)
+                        base_obj = Object.const_get(const_name)
+                    else
+                        super(from, const_name)
+                        base_obj = Object.const_get(const_name)
+                    end
+                end
+
+                if base_obj
                     if defined? base_obj.restricted? and base_obj.restricted?
 
                         c = Class.new(base_obj)
                         c.acts_as_restricted_set_options[:use_restriction] = true
-                        return Restricted.const_set(const_name, c)
+                        return c
 
                     end
                 end
 
                 super(from, const_name)
-            rescue
-                return # hack ?
             end
        end
 
