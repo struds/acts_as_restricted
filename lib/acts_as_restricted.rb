15af271094f273e76091a75db85f1218752c92bc
Stephen Strudwick <struds@Macintosh-2.local>
2008-12-04 16:01:35 +0000

    set instantiated results to readonly is restrictions on write for user

--- lib/acts_as_restricted.rb
+++ lib/acts_as_restricted.rb
@@ -10,9 +10,8 @@ module ActsAsRestricted
     module SingletonMethods
 
         def acts_as_restricted(options = {})
-            attr_accessor :is_restricted
 
-            options = { :read => true, :write => true, :use_restriction => true }.merge(options)
+            options = { :read => true, :write => true }.merge(options)
 
             write_inheritable_attribute :acts_as_restricted_set_options, options
             class_inheritable_reader :acts_as_restricted_set_options
@@ -20,17 +19,14 @@ module ActsAsRestricted
             include InstanceMethods
             extend ClassMethods
 
-            # todo: we need instantiate any records with restricted == true if called from restricted named_scope
-# overwrite find_every to set restricted on every instantiate object where options[:is_restricted]
-
-            named_scope :restricted, lambda { {:conditions => condition, :joins => join, :select => select, :readonly => true} }
+            if acts_as_restricted_set_options[:write] == false
+                named_scope :restricted, lambda { {:conditions => condition, :joins => join, :select => select, :readonly => true} }
+            else
+                named_scope :restricted, lambda { {:conditions => condition, :joins => join, :select => select, :readonly => restricted_write} }
+            end
         end
 
         module InstanceMethods
-
-	    def is_restricted?
-                is_restricted
-	    end
 
             def restricted?
                 return true
@@ -47,12 +43,6 @@ module ActsAsRestricted
         end
 
         module ClassMethods
-
-def find_every(options)
-    records = super(options)
-    records.each { |record| record.is_restricted = true } if options[:readonly]
-    records
-end
 
             def current_user
                 if Thread.current['user']
@@ -76,9 +66,12 @@ end
                 return nil
             end
 
+            def restricted_write
+                return false
+            end
+
             def join
-                if acts_as_restricted_set_options[:use_restriction] == false or
-                   acts_as_restricted_set_options[:read] == false
+                if acts_as_restricted_set_options[:read] == false
                        return nil
                 else
                     restricted_join
@@ -86,8 +79,7 @@ end
             end
 
             def condition
-                if acts_as_restricted_set_options[:use_restriction] == false or
-                   acts_as_restricted_set_options[:read] == false
+                if acts_as_restricted_set_options[:read] == false
                        return SQL_NO_RESTRICTIONS # effectively do nothing
                 else
                     restricted_condition
@@ -95,8 +87,7 @@ end
             end
 
             def select
-                if acts_as_restricted_set_options[:use_restriction] == false or
-                   acts_as_restricted_set_options[:read] == false
+                if acts_as_restricted_set_options[:read] == false
                        return nil
                 else
                     restricted_select
